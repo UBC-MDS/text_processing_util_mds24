f7ab4a7a7c52d6c25ec4e1a68e10499670c76070
Yu Lun Lee <allan83.9.2@hotmail.com>
2024-01-09 15:59:38 -0800

    added docstring for text preprocess function

--- src/text_processing_util_mds24/text_processing_util_mds24.py
+++ src/text_processing_util_mds24/text_processing_util_mds24.py
@@ -1,7 +1,24 @@
 def text_clean(docs: list[str]) -> list[str]:
-    # remove punctuations
-    # lowercase everything
-    # remove numbers
+    """Removes punctiations, make everything lower case and remove numbers
+       in documents.
+
+    Parameters
+    ----------
+    docs : list[str]
+        Documents to be processed.
+        Each item item in the list is a document.
+
+    Returns
+    -------
+    list[str]
+        Cleaned documents.
+
+    Examples
+    --------
+    >>> text_clean(["we are group 10",
+                    "we are the best"])
+    """
+    pass
 
 
 def frequency_vectorizer(docs: list[str]) -> list[dict[str:float]]:
