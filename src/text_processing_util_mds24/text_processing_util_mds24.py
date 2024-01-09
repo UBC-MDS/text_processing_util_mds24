83345009e8785cf8c3135c133c7c1c913d3513c9
Jerry Yu <jyu565@gmail.com>
2024-01-08 17:14:15 -0800

    added function prototype

---
+++ src/text_processing_util_mds24/text_processing_util_mds24.py
@@ -0,0 +1,18 @@
+def text_clean(docs: list[str]) -> list[str]:
+    # remove punctuations
+    # lowercase everything
+    # remove numbers
+
+
+def frequency_vectorizer(docs: list[str]) -> list[dict[str:float]]:
+    pass
+
+
+def tfidf_vectorizer(docs: list[str]) -> list[dict[str:float]]:
+    pass
+
+
+def tokenizer_padding(docs: list[str]) -> list[list[int]]:
+    pass
+
+
