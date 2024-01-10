996ae16d16eb63bdbb99f767a7595b4da4215cee
Jerry Yu <jyu565@gmail.com>
2024-01-10 07:04:01 -0800

    added docstring for tokenizer_padding

--- src/text_processing_util_mds24/text_processing_util_mds24.py
+++ src/text_processing_util_mds24/text_processing_util_mds24.py
@@ -13,6 +13,27 @@ def tfidf_vectorizer(docs: list[str]) -> list[dict[str:float]]:
 
 
 def tokenizer_padding(docs: list[str]) -> list[list[int]]:
+    """
+    Converts each text document into a list of numerical tokens, and pads
+    shorter sequences so that each tokenized document has the same length.
+
+    Parameters
+    ----------
+    docs : list[str]
+        A list of text documents.
+
+    Returns
+    -------
+    list[list[int]]
+        A list of tokenized and padded sequences of the input documents.
+
+    Examples
+    --------
+    >>> tokenizer_padding(["the first sentence", "the second longer sentence"])
+    [[1, 2, 3, 0], [1, 4, 5, 3]]
+    >>> tokenizer_padding(["a sample text", "sample text two"])
+    [[1, 2, 3], [2, 3, 4]]
+    """
     pass
 
 
