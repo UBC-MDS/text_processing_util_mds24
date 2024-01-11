cfe74ef6df7aea2c917bff5148db7e1db9a5437d
MoNorouzi23 <masnorouzii@gmail.com>
2024-01-10 16:39:12 -0800

    adding docstring

--- src/text_processing_util_mds24/text_processing_util_mds24.py
+++ src/text_processing_util_mds24/text_processing_util_mds24.py
@@ -9,10 +9,30 @@ def frequency_vectorizer(docs: list[str]) -> list[dict[str:float]]:
 
 
 def tfidf_vectorizer(docs: list[str]) -> list[dict[str:float]]:
+
+    """
+    Calculate TF-IDF scores for a list of documents.
+
+    Parameters
+    ----------
+    docs : list[str]: 
+        List of documents (strings)
+
+    Returns
+    -------
+    list[dict[str:float]]
+        List of dictionaries containing TF-IDF scores for each term in each document.
+
+    Examples
+    --------
+    >>> tfidf_vectorizer(["This is the first document."])
+    
+    [{'This': -0.13862943611198905, 'is': -0.13862943611198905, 
+    'the': -0.13862943611198905, 'first': -0.13862943611198905, '
+    'document.': -0.13862943611198905}]
+    """
     pass
 
 
 def tokenizer_padding(docs: list[str]) -> list[list[int]]:
     pass
-
-
