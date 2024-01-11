eefd889f5962029fc5cdb5d46fc01367f02a0ec5
nassim9 <nassim9@student.ubc.ca>
2024-01-10 21:18:26 -0800

    Frequency_vectorizer added

--- src/text_processing_util_mds24/text_processing_util_mds24.py
+++ src/text_processing_util_mds24/text_processing_util_mds24.py
@@ -5,6 +5,27 @@ def text_clean(docs: list[str]) -> list[str]:
 
 
 def frequency_vectorizer(docs: list[str]) -> list[dict[str:float]]:
+    """
+    Calculates the frequency of each word in a list of text documents.
+
+    Parameters
+    ----------
+    docs : list[str]
+        A list of text documents.
+
+    Returns
+    -------
+    list[dict[str: float]]
+        A list of dictionaries, where each dictionary contains the word and the word's frequency in a text document.
+        
+    Examples
+    --------
+    >>> documents = ["This is a sample document.", "Another document for testing."]
+    >>> result = frequency_vectorizer(documents)
+    >>> print(result)
+    [{'This': 0.14, 'is': 0.14, 'a': 0.14, 'sample': 0.14, 'document.': 0.14},
+    {'Another': 0.16, 'document': 0.16, 'for': 0.16, 'testing.': 0.16}]
+    """
     pass
 
 
