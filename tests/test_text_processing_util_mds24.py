4deb7e9b99d30a14772eed9db76a044c8057931b
Jerry Yu <jyu565@gmail.com>
2024-01-15 14:43:57 -0800

    simple setup for test file

--- tests/test_text_processing_util_mds24.py
+++ tests/test_text_processing_util_mds24.py
@@ -1,1 +1,27 @@
 from text_processing_util_mds24 import text_processing_util_mds24
+
+empty_list = []
+list_empty_str = [""]
+one_doc = ["This is a document with 1 string."]
+list_invalid = [12, 33.5, None]
+list_mixed_empty = ["Here is document one.", "", "we have document 2"]
+list_num_punctuation = ["112.32", "!@#$", "795"]
+
+
+
+# text_clean
+
+
+
+# frequency_vectorizer
+
+
+
+# tfidf_vectorizer
+
+
+
+# tokenizer_padding
+
+
+
